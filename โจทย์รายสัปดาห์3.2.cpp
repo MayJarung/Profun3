efbe0f617af6deab317f700a2104ded03f9f0791
May Jarung <mayjarung122545@gmail.com>
2021-09-02 01:39:00 +0700

    Create โจทย์รายสัปดาห์3.2.cpp

---
+++ โจทย์รายสัปดาห์3.2.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ 
+int main()
+{   
+    int loop,input,i,max;
+     
+    printf(" Enter number for loop : ");
+    scanf("%d", &loop);
+    printf("\n");
+     
+    for(i = 0; i < loop; i++)
+    {
+        printf(" Number #%d : ", i+1);
+        scanf("%d", &input);
+         
+        if(i==0 || input > max)
+        {
+            max = input;
+        }
+    }
+    printf("\n Maximum is %d\n\n", max);
+     
+    return 0;
+}
+    
